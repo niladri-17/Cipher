95aa78a9361cf406c68459bb1f41bf6ecc49d822
niladri-17 <niladri.basak.007@gmail.com>
2025-02-24 13:10:51 +0530

    allowed origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,16 +9,44 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://projects.niladribasak.in",
   "https://projects.niladribasak.in/cipher",
+  "https://www.projects.niladribasak.in/cipher",
   "https://cipher-mern-chat-g9wrwtj5n-niladris-projects-4a9a6d43.vercel.app",
 ];
 
+// app.use(
+//   cors({
+//     // origin: process.env.CORS_ORIGIN, // allow to server to accept request from different origin
+//     origin: allowedOrigins,
+//     credentials: true, // allows cookies, authorization headers, etc to be passed from client
+//   })
+// );
+
 app.use(
   cors({
-    // origin: process.env.CORS_ORIGIN, // allow to server to accept request from different origin
-    origin: allowedOrigins,
-    credentials: true, // allows cookies, authorization headers, etc to be passed from client
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Required for cookies/auth
   })
 );
+
+// Optional: Add headers manually for extra safety
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://projects.niladribasak.in");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+// Handle preflight OPTIONS request
+app.options("*", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
